80a2ab3c57fcec81826b23b310c1c0a7286b0123
Alfredo Palacios <joseaph.1998@gmail.com>
2021-01-25 18:26:51 -0600

    fortify modificado

--- config/fortify.php
+++ config/fortify.php
@@ -131,7 +131,6 @@ return [
 
     'features' => [
         Features::registration(),
-        Features::resetPasswords(),
         Features::emailVerification(),
         Features::updateProfileInformation(),
         Features::updatePasswords(),
