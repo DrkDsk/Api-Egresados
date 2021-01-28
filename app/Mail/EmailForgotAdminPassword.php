ff224fa7e05fe1a99317be00b9ca5e6127fcc653
Alfredo Palacios <joseaph.1998@gmail.com>
2021-01-28 11:13:37 -0600

    remover implementación

--- app/Mail/EmailForgotAdminPassword.php
+++ app/Mail/EmailForgotAdminPassword.php
@@ -7,7 +7,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 
-class EmailForgotAdminPassword extends Mailable implements ShouldQueue
+class EmailForgotAdminPassword extends Mailable
 {
     use Queueable, SerializesModels;
     protected $url;
